d7f3d7fe56f87a7e22327736482de8f97f547c2f
Mike Thurgood <mikecthurgood@gmail.com>
2019-09-24 13:25:55 +0100

    saving for future use

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,32 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, through: :listings
+
+  def city_openings(start_date, end_date)
+    date_range = (Date.parse(start_date)..Date.parse(end_date))
+    listings.map do |listing|
+      available = true
+      listing.booked_dates.each do |date|
+        if date_range === date
+          available = false
+        end
+      end
+      listing if available
+    end
+  end
+
+  def ratio_res_to_listings
+    self.reservations.length.to_f / self.listings.length
+  end
+
+  def self.highest_ratio_res_to_listings
+    self.all.max_by{|city| city.ratio_res_to_listings}
+  end
+
+  def self.most_res
+    self.all.max_by{|city| city.reservations.length}
+  end
 
 end
 
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,56 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, :checkout, presence: true
+  validate :host_is_not_guest
+  validate :only_book_if_available
+  validate :checkin_before_checkout?
+
+  def duration
+    (self.checkout - self.checkin).to_i
+  end
+
+  def total_price
+    listing.price * self.duration
+  end
+  
+  
+  private
+  
+  def host_is_not_guest
+    if self.guest_id != self.listing.host_id
+      true
+    else
+    errors.add(:guest, "You can't book your own place you fool!")
+    end
+  end
+
+  def only_book_if_available 
+    date_range = (self.checkin..self.checkout)
+    self.listing.booked_dates.each do |date|
+      if date_range === date
+        errors.add(:dates, "Sorry, this listing is not available on these dates")
+      else
+        true
+      end
+    end
+  end
+
+  def checkin_before_checkout?
+    if self.checkin >= self.checkout
+      errors.add(:dates, "You can't leave before you arrive!")
+    else
+      true
+    end
+  end
+
+  def checkin_and_checkout?
+    if self.checkin.blank? || self.checkout.blank?
+      errors.add(:dates, "You must fill in checkin and checkout")
+    else
+      true
+    end
+  end
+
+
 end
